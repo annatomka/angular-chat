5b34857dc92aad8194398bdf32e8609bf6a734e6
Tomka Anna <tomka.anna@bmeautsoft.hu>
2015-09-22 02:00:34 +0200

    remove room list

--- src/app/room/room.service.js
+++ src/app/room/room.service.js
@@ -7,28 +7,12 @@
 
   /** @ngInject */
   function RoomService($resource, apiUrl,Room, RoomUser) {
-    //var Room =  $resource(apiUrl + '/rooms/:id');
-    this.getRooms = getRooms;
     this.createRoom = createRoom;
-    this.getUsers = getUsers;
-
-    function getRooms() {
-
-      var rooms = Room.query(function (data) {
-        console.log(rooms);
-        console.log(data)
-      });
-      return rooms;
-    }
 
     function createRoom(room){
       //var Room =  $resource(apiUrl + '/rooms/:id');
       var newRoom = new Room({name : room.name});
       return newRoom.$save();
-    }
-
-    function getUsers(roomId) {
-      return RoomUser.query({id: roomId});
     }
   }
 
